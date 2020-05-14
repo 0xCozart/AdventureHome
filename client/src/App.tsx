7249c5a612cf65eea1c37d629b069e11f6272cd1
Alan <alanvega002@gmail.com>
2020-05-14 12:58:27 -0800

    autoconnect functionality added

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -44,36 +44,8 @@ function App() {
   };
 
   useEffect(() => {
-    try {
-      connectMetamask();
-    } catch (e) {
-      console.log("metamask on autoconnect:", e);
-    }
-  });
-  // useEffect(() => {
-  //   (async () => {
-  //     try {
-  //       if (ethereum) {
-  //         web3 = new Web3(ethereum);
-  //         try {
-  //           await ethereum.enable();
-  //           web3.eth.getAccounts((err: string, accounts: string[]) => {
-  //             if (err) console.log(err);
-  //             else if (!accounts.length) alert("No Metamask accounts found");
-  //             else {
-  //               setUserAccount(accounts[0]);
-  //               setIsConnected(true);
-  //             }
-  //           });
-  //         } catch (e) {
-  //           console.error("Error, ", e);
-  //         }
-  //       }
-  //     } catch (e) {
-  //       console.log("error", e);
-  //     }
-  //   })();
-  // });
+    connectMetamask();
+  }, []);
 
   useEffect(() => {
     (async () => {
