0190a3c9474444942670eaa95d35b5d9da6a3c55
Alan <alanvega002@gmail.com>
2020-05-18 22:40:57 -0800

    renamed metamask to web3

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,7 +3,7 @@ import { tokenData } from "./data/tokenData";
 import { TknTable } from "./components/TknTable";
 import { balanceObj } from "./@types/tokenStyles";
 import { Explorer, sites } from "adventure-component-library";
-import determineBalance from "./ethereum/determineBalance";
+import determineBalance from "./web3/balance";
 import { BalanceRequest } from "./components/QueryBar";
 import Button from "react-bootstrap/Button";
 import "bootstrap/dist/css/bootstrap.min.css";
